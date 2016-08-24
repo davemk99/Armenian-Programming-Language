5b1d5d04f2558fe56b84ae2cf395844e8a088b00
davem <davem@DESKTOP-F35DM1F>
2016-08-24 15:45:58 +0400

    Name Changes

--- SimpleCompiler/Properties/AssemblyInfo.cs
+++ SimpleCompiler/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleCompiler")]
+[assembly: AssemblyTitle("Armenian Programming Langauge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleCompiler")]
+[assembly: AssemblyProduct("Armenian Programming Language")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
